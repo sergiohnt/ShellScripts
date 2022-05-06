06eefa9c07e3dcbefb0a78bb1cff7d7738e220c2
Sergio Silva <s.silva@ecotra.info>
2022-05-06 18:54:30 -0300

    Criando Home Dir

--- user_manager_ubuntu.sh
+++ user_manager_ubuntu.sh
@@ -115,7 +115,7 @@ case $RESP in
 		if [ "$VERIFICALOGIN" != "$LOGIN" ]
 		    then
 		    	# Cria usuario com LOGIN, SENHA, COMENTARIO e forca alteracao de SENHA no primeiro LOGIN
-		    	useradd -c "$NAME" -G sudo -p $SENHA $LOGIN
+		    	useradd -c -m "$NAME" -G sudo -p $SENHA $LOGIN
 		    	echo "$LOGIN:$SENHA" > /root/Ltemp 
 					chpasswd < /root/Ltemp
 		    	passwd -e $LOGIN
